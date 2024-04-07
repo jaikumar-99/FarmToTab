5dfb9acd70f6c948a477ebf967795f166b9f3d91
jaikumar-99 <jaikumarg888@gmail.com>
2024-04-07 00:42:44 -0400

    Fixed PrimeNg Errors

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Table } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
 // import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+  styleUrl: './users.component.scss'    
 })
 export class UsersComponent {
   customers: any;
@@ -16,12 +18,27 @@ export class UsersComponent {
 
   statuses: any;
 
-  loading: boolean = true;
+  loading: boolean = false;
+  searchdata: any = null;
 
   activityValues: number[] = [0, 100];
+
   constructor() { }
   ngOnInit() {
-    this.users = [{ name: 'Abhi', State: 'Karnataka', utype: 'Farmer', status: 'Active' }, { name: 'Jai', State: 'Andhra Pradesh', utype: 'Farmer', status: 'Active' }, { name: 'Abhishek', state: 'Andhra Pradesh', utype: 'Consumer', status: 'Inactive' }];
+    this.users = [{ name: 'Abhi', state: 'Karnataka', utype: 'Farmer', status: 'Active' }, { name: 'Jai', state: 'Andhra Pradesh', utype: 'Farmer', status: 'Active' }, { name: 'Abhishek', state: 'Andhra Pradesh', utype: 'Consumer', status: 'Inactive' }];
   }
+
+
+  clear(table: Table) {
+    table.clear();
+    this.searchdata = null;
+  }
+
+checkclear(event: any, dt: any) {
+  this.searchdata = event;
+  console.log(event,dt);
+}
+
+
 
 }
