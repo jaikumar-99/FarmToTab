1032f6886b290e4f613c747589e417dac5dd9fcd
Ryuytyuu-Greseven <rukku93223@gmail.com>
2024-04-08 00:08:12 +0530

    Prodile design!

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+
+
+  // profile
+  profileDetails: any = {};
+
+  constructor(private appService: AppService) { }
+
+  ngOnInit(): void {
+    this.profileDetails = this.appService.profileDetails;
+  }
 
 }
